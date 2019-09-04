5df0ed2ff69bd959321e762d5a089cceee64af29
Seung Hun Han <shhan730@gmail.com>
2019-09-04 20:55:42 +0900

    Initial Commit

---
+++ wageulSeoul/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wageulSeoul
+//
+//  Created by HSH on 04/09/2019.
+//  Copyright © 2019 HSH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
