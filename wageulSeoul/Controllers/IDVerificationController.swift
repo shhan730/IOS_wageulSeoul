9cbe48356826eb32ae170cb1175daa3dde81a177
Seung Hun Han <shhan730@gmail.com>
2019-09-08 04:45:04 +0900

    Add IDVerification Files

---
+++ wageulSeoul/Controllers/IDVerificationController.swift
@@ -0,0 +1,31 @@
+//
+//  IDVerificationController.swift
+//  wageulSeoul
+//
+//  Created by HSH on 08/09/2019.
+//  Copyright © 2019 HSH. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IDVerificationController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
---
+++ wageulSeoul/Models/IDVerificationInfo.swift
@@ -0,0 +1,17 @@
+//
+//  IDVerificationInfo.swift
+//  wageulSeoul
+//
+//  Created by HSH on 08/09/2019.
+//  Copyright © 2019 HSH. All rights reserved.
+//
+
+import Foundation
+
+struct IDVerificationInfo {
+    
+    var isAuthrized = false
+    
+    
+    
+}
